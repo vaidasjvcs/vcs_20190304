7f8727115decd56a80378612ab48e53273a5524f
T0MMEI <t.meigys@gmail.com>
2019-04-19 21:00:44 +0300

    method added

--- src/main/java/com/mycompany/app/TomasM.java
+++ src/main/java/com/mycompany/app/TomasM.java
@@ -1,5 +1,13 @@
 package com.mycompany.app;
 
 public class TomasM {
+	
 
+	public String metodas (String name) {
+		name = "Julia Roberts";
+		String sayHello = "Hello, " + name + "I'm Tomas, its nice to meet you";
+		
+	return sayHello;
+	
+	}
 }
---
+++ src/test/java/com/mycompany/app/TomasT_Test.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TomasT_Test {
+	
+	@Test
+	public void test_metodes_returnsName() {
+		//test
+		String name = "Julia Roberts";
+		
+		//asserts
+		assertEquals("Bananas", name);
+		
+	}
+
+}
